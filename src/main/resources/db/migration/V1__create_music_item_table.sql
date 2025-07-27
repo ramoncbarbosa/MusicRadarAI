20884c88cf17feee725c9425df414a2fb68320ab
ramoncbarbosa <ramonsengineer.oficial@gmail.com>
2025-07-27 12:23:30 -0300

    fix: correct music table definition

--- src/main/resources/db/migration/V1__create_music_item_table.sql
+++ src/main/resources/db/migration/V1__create_music_item_table.sql
@@ -1,7 +1,7 @@
-CREATE TABLE tb_musics {
+CREATE TABLE tb_musics (
     id VARCHAR(36) NOT NULL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     artist VARCHAR(255) NOT NULL,
     releaseDate INTEGER,
     genre VARCHAR(255) NOT NULL
-};
+);
