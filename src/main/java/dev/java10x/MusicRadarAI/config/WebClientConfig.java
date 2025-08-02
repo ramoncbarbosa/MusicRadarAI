855e718e07fb324e5b5b77738a847438c4f98e24
ramoncbarbosa <ramonsengineer.oficial@gmail.com>
2025-08-02 13:05:03 -0300

    fix: adjust url

--- src/main/java/dev/java10x/MusicRadarAI/config/WebClientConfig.java
+++ src/main/java/dev/java10x/MusicRadarAI/config/WebClientConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
-    @Value("${gemini.api.url:https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent}")
+    @Value("${openai.api.url:https://api.openai.com/v1/responses}")
     private String aiClientURL;
 
     @Bean
